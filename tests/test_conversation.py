a1a90fa01f0f1d2dea288f5bb9bd92c32dbe9a76
Allen Porter <allen.porter@gmail.com>
2024-06-02 00:58:13 +0000

    Update custom conversation snapshot tests

--- tests/test_conversation.py
+++ tests/test_conversation.py
@@ -35,7 +35,9 @@ def freeze_the_time():
 
 
 @pytest.fixture(autouse=True)
-async def mock_setup_integration(hass: HomeAssistant, mock_config_entry: MockConfigEntry) -> None:
+async def mock_setup_integration(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry
+) -> None:
     """Setup the integration"""
     assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
@@ -103,9 +105,7 @@ async def test_conversation_entity(
     assert mock_create.mock_calls[0][2]["messages"] == snapshot
 
 
-@patch(
-    f"custom_components.{DOMAIN}.conversation.llm.AssistAPI._async_get_tools"
-)
+@patch(f"custom_components.{DOMAIN}.conversation.llm.AssistAPI._async_get_tools")
 async def test_function_call(
     mock_get_tools,
     hass: HomeAssistant,
@@ -221,9 +221,7 @@ async def test_function_call(
     )
 
 
-@patch(
-    f"custom_components.{DOMAIN}.conversation.llm.AssistAPI._async_get_tools"
-)
+@patch(f"custom_components.{DOMAIN}.conversation.llm.AssistAPI._async_get_tools")
 async def test_function_exception(
     mock_get_tools,
     hass: HomeAssistant,
@@ -409,4 +407,4 @@ async def test_unknown_hass_api(
         hass, "hello", None, Context(), agent_id=mock_config_entry.entry_id
     )
 
-    assert result == snapshot
+    assert result.as_dict() == snapshot
